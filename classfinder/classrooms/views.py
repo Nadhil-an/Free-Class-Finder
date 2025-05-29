70fb0ff79a1c2fbf27ccbcb4059f6d50b64847bd
Nadhil-an <nadhil.edu@gmail.com>
2025-05-30 02:08:07 +0530

    implement find the free class based on the floors as well

--- classfinder/classrooms/views.py
+++ classfinder/classrooms/views.py
@@ -8,11 +8,13 @@ def login(request):
     times = ['9-10 AM', '10-11AM', '11-12PM', '12-1PM', '1-2PM', '2-3PM', '3-4PM', '4-5PM']
 
     blocks =FreeClass.objects.values_list('Block',flat=True).distinct()
+    floors = FreeClass.objects.values_list('Floor',flat=True).distinct()
 
     context = {
         'days':days,
         'times':times,
-        'blocks':blocks
+        'blocks':blocks,
+        'floors':floors
     }
     return render(request,'login.html',context)
 
@@ -20,10 +22,11 @@ def searchclass(request):
     block = request.GET.get('block')
     day   = request.GET.get('day')
     time  = request.GET.get('time')
+    floor = request.GET.get('floor')
 
-    if block and day and time:
-        classes = FreeClass.objects.filter(Block=block, Day=day, Time=time,is_occupied=True).order_by('Room_No')
-        context = {'classes': classes, 'block': block, 'day': day, 'time': time}
+    if block and day and time and floor:
+        classes = FreeClass.objects.filter(Block=block, Day=day, Time=time,Floor=floor,is_occupied=True).order_by('Room_No')
+        context = {'classes': classes, 'block': block, 'day': day, 'time': time,'floor':floor}
         return render(request, 'base.html', context)
 
     return redirect('login')
@@ -32,10 +35,11 @@ def occupied_classes(request):
     block = request.GET.get('block')
     day = request.GET.get('day')
     time = request.GET.get('time')
+    floor = request.GET.get('floor')
 
-    if block and day and time:
-        classes = FreeClass.objects.filter(Block=block, Day=day, Time=time,is_occupied=False).order_by('Room_No')
-        context = {'classes': classes, 'block': block, 'day': day, 'time': time}
+    if block and day and time and floor:
+        classes = FreeClass.objects.filter(Block=block, Day=day, Time=time,Floor=floor,is_occupied=False).order_by('Room_No')
+        context = {'classes': classes, 'block': block, 'day': day, 'time': time,'floor':floor}
         return render(request, 'base.html', context)
     else:
         return redirect('searchclass')
@@ -45,10 +49,11 @@ def available_classes(request):
     block = request.GET.get('block')
     day = request.GET.get('day')
     time = request.GET.get('time')
+    floor = request.GET.get('floor')
 
-    if block and day and time:
-        classes = FreeClass.objects.filter(Block=block,Day=day,Time=time,is_occupied=True).order_by('Room_No')
-        context = {'classes': classes, 'block': block, 'day': day, 'time': time}
+    if block and day and time and floor:
+        classes = FreeClass.objects.filter(Block=block,Day=day,Time=time,Floor=floor,is_occupied=True).order_by('Room_No')
+        context = {'classes': classes, 'block': block, 'day': day, 'time': time,'floor':floor}
         return render(request, 'base.html', context)
     else:
         return redirect('searchclass')
@@ -57,10 +62,11 @@ def all_classes(request):
     block = request.GET.get('block')
     day = request.GET.get('day')
     time = request.GET.get('time')
+    floor = request.GET.get('floor')
 
-    if block and day and time:
-        classes = FreeClass.objects.filter(Block=block,Day=day,Time=time).order_by('Room_No')
-        context = {'classes': classes, 'block': block, 'day': day, 'time': time}
+    if block and day and time and floor:
+        classes = FreeClass.objects.filter(Block=block,Day=day,Time=time,Floor=floor).order_by('Room_No')
+        context = {'classes': classes, 'block': block, 'day': day, 'time': time,'floor':floor}
         return render(request, 'base.html', context)
     else:
         return redirect('searchclass')
