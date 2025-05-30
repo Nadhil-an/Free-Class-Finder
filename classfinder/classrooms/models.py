f40d1ec23c455c727ab87e8c7b9756093c285967
Nadhil-an <nadhil.edu@gmail.com>
2025-05-30 16:03:55 +0530

    make change in classrooms models

--- classfinder/classrooms/models.py
+++ classfinder/classrooms/models.py
@@ -4,62 +4,62 @@ class FreeClass(models.Model):
     
     
 
-    # DAY_CHOICES = (
-    #     ('Monday','Monday'),
-    #     ('Tuesday','Tuesday'),
-    #     ('Wednesday','Wednesday'),
-    #     ('Thursday','Thursday'),
-    #     ('Friday','Friday'),
-    # )
+    DAY_CHOICES = (
+        ('Monday','Monday'),
+        ('Tuesday','Tuesday'),
+        ('Wednesday','Wednesday'),
+        ('Thursday','Thursday'),
+        ('Friday','Friday'),
+    )
     
 
-    # TIME_CHOICES = (
-    #     ('9-10am','9-10am'),
-    #     ('10-11am','10-11am'),
-    #     ('11-12am','11-12am'),
-    #     ('12-1pm','12-1pm'),
-    #     ('1-2pm','1-2pm'),
-    #     ('2-3pm','2-3pm'),
-    #     ('3-4pm','3-4pm'),
-    #     ('4-5pm','4-5pm'),
-    # )
+    TIME_CHOICES = (
+        ('9-10am','9-10am'),
+        ('10-11am','10-11am'),
+        ('11-12am','11-12am'),
+        ('12-1pm','12-1pm'),
+        ('1-2pm','1-2pm'),
+        ('2-3pm','2-3pm'),
+        ('3-4pm','3-4pm'),
+        ('4-5pm','4-5pm'),
+    )
     
 
-    # def __str__(self):
-    #     return f"{self.Block} | {self.Day} | {self.Time}"
+    def __str__(self):
+        return f"{self.Block} | {self.Day} | {self.Time}"
     
-    # STATUS_CHOICE =(
-    #     ('Available','Available'),
-    #     ('Occupied','Occupied')
-    # )
+    STATUS_CHOICE =(
+        ('Available','Available'),
+        ('Occupied','Occupied')
+    )
 
-    # FLOOR_CHOICE=(
-    #     ('Ground Floor','Ground Floor'),
-    #     ('1 Floor','1 Floor'),
-    #     ('2 Floor','2 Floor'),
-    #     ('3 Floor','3 Floor'),
-    #     ('4 Floor','4 Floor'),
-    #     ('5 Floor','5 Floor'),
-    #     ('6 Floor','6 Floor'),
-    #     ('7 Floor','7 Floor'),
-    #     ('8 Floor','8 Floor'),
-    #     ('9 Floor','9 Floor'),
-    #     ('10 Floor','10 Floor'),
-    # )
+    FLOOR_CHOICE=(
+        ('Ground Floor','Ground Floor'),
+        ('1 Floor','1 Floor'),
+        ('2 Floor','2 Floor'),
+        ('3 Floor','3 Floor'),
+        ('4 Floor','4 Floor'),
+        ('5 Floor','5 Floor'),
+        ('6 Floor','6 Floor'),
+        ('7 Floor','7 Floor'),
+        ('8 Floor','8 Floor'),
+        ('9 Floor','9 Floor'),
+        ('10 Floor','10 Floor'),
+    )
 
     
-    # TYPE_CHOICE = (
-    #     ('classroom','Classroom'),
-    #     ('Computer Lab','Computer Lab')
-    # )
+    TYPE_CHOICE = (
+        ('classroom','Classroom'),
+        ('Computer Lab','Computer Lab')
+    )
 
     Block = models.CharField(max_length=10)
-    Day = models.CharField(max_length=20 )
-    Time= models.CharField(max_length=10)
+    Day = models.CharField(max_length=20,choices=DAY_CHOICES)
+    Time= models.CharField(max_length=10,choices=TIME_CHOICES)
     Floor = models.CharField(max_length=20)
     Room_No = models.CharField(max_length=10)
-    Room_Status = models.CharField(max_length=20)
-    Room_Type = models.CharField(max_length=20)
+    Room_Status = models.CharField(max_length=20,choices=FLOOR_CHOICE)
+    Room_Type = models.CharField(max_length=20,choices=TYPE_CHOICE)
     is_occupied = models.BooleanField(default=False)
 
     def __str__(self):
